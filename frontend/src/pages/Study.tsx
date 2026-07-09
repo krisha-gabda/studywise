507ff197b2b28d6ecc5314021fffd02b9ce13a69
g53032kg <krisha.gabda@student.manchester.ac.uk>
2026-07-09 17:07:51 +0530

    Finished the main part of the summary

--- frontend/src/pages/Study.tsx
+++ frontend/src/pages/Study.tsx
@@ -47,10 +47,27 @@ export default function Study() {
     if (error) return <p>An error occured. Please try again.</p>
 
     return(
-        <div className="bg-page-bg  min-h-screen flex flex-col justify-center items-center">
-            <h3 className="text-primary-text font-bold text-5xl p-12">{topic.name}</h3>
-            <p className="text-primary-text">{summary.headline}</p>
-            <p className="text-secondary-text">{summary.summary}</p>
+        <div className="bg-page-bg  min-h-screen">
+            <h3 className="text-primary-text font-bold text-5xl p-12 pb-6">{topic.name}</h3>
+            <hr className=" border-secondary-text mx-12"/>
+            <p className="text-primary-text px-12 pt-12 text-2xl ">{summary.headline}</p>
+            <p className="text-secondary-text px-12 pt-2">{summary.summary}</p>
+
+            <p className="text-primary-text text-3xl p-12 font-bold">Study This Topic</p>
+            <div className="flex flex-row  justify-center items-center gap-4 px-12 pb-12">
+                <div className="bg-card-bg w-full p-12  border-t-primary border-t-4">
+                    <p className="text-2xl text-primary-text font-bold pb-12">Flashcards</p>
+                    <p className="text-secondary-text">Strengthen your understanding by reviewing flashcards</p>
+                    <p className="text-secondary-text">Revise like a pro</p>
+                    <button className="bg-primary p-4 rounded-lg mt-8 text-primary-text font-bold hover:bg-primary-hover transition-all ease-in cursor-pointer">Start Revising</button>
+                </div>
+                <div className="bg-card-bg w-full p-12  border-t-primary border-t-4">
+                    <p className="text-2xl text-primary-text font-bold pb-12">Take Quiz</p>
+                    <p className="text-secondary-text">Test yourself to check your understanding</p>
+                    <p className="text-secondary-text">Test your understanding</p>
+                    <button className="bg-primary p-4 rounded-lg mt-8 text-primary-text font-bold hover:bg-primary-hover transition-all ease-in cursor-pointer">Start Quiz</button>
+                </div>
+            </div>
         </div>
     )
 }
